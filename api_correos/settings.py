137ddb5f7d98cc6b678bacb6884f65f5da136920
NicolasFG <nicolas.figueroa@utec.edu.pe>
2023-10-29 16:06:52 -0500

    cambios 29102023

--- api_correos/settings.py
+++ api_correos/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
                  'interaccion-correros.azurewebsites.net',
-                 'http://127.0.0.1:8000/',
                  '127.0.0.1', 
                  'localhost',
                 ]
